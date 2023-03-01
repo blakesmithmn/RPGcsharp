e9bdcb14fc2b840d81556173fe6c97d92afc6978
blakesmithmn <blake.smith.mn@gmail.com>
2023-02-28 19:01:09 -0600

    added properties with Lists for loot, as well as player quest log and completion status.

--- Engine/Monster.cs
+++ Engine/Monster.cs
@@ -18,6 +18,8 @@ namespace Engine
         public int RewardExperiencePoints { get; set; }
         public int RewardGold { get; set; }
 
+        public List<LootItem> LootTable { get; set; }
+
         public Monster(int id, string name, int maximumDamage, int rewardExperiencePoints, int rewardGold, int currentHitPoints, int maximumHitPoints) : base(currentHitPoints, maximumHitPoints)
         {
             ID = id;
@@ -25,6 +27,7 @@ namespace Engine
             MaximumDamage = maximumDamage;
             RewardExperiencePoints = rewardExperiencePoints;
             RewardGold = rewardGold;
+            LootTable = new List<LootItem>();
         }
     }
 }
--- Engine/Player.cs
+++ Engine/Player.cs
@@ -12,15 +12,20 @@ namespace Engine
 
         // public int CurrentHitPoints { get; set; }
         //  public int MaximumHitPoints { get; set; }
+       
         public int Gold { get; set; }
         public int ExperiencePoints { get; set; }
-        public int Level { get; set; }  
+        public int Level { get; set; }
+        public List<InventoryItem> Inventory { get; set; }
+        public List<PlayerQuest> Quests { get; set; }
 
         public Player(int currentHitPoints, int maximumHitPoints, int gold, int experiencepoints, int level) : base(currentHitPoints, currentHitPoints)
         {
             Gold = gold;
             ExperiencePoints = experiencepoints;
             Level = level;
+            Inventory = new List<InventoryItem>();
+            Quests = new List<PlayerQuest>();
         }
 
     }
